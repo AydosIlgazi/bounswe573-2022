a0015ab6a1e14a0d57acc4128814adc5af818456
aydos <aydos@DESKTOP-69DNI12>
2022-03-12 22:47:00 +0300

    Database Connection; Fixes #4

--- Swe573/settings.py
+++ Swe573/settings.py
@@ -73,10 +73,15 @@ WSGI_APPLICATION = 'Swe573.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+
+# settings.py
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'swedb',
+        'USER': 'root',
+        'HOST': '127.0.0.1',
+        'PORT': '3306'
     }
 }
 
