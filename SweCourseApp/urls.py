71e68c42cd89bde3a7b048245aef1bb5f56e6ded
aydos <aydos@DESKTOP-69DNI12>
2022-05-25 22:49:22 +0300

    participants page url added

--- SweCourseApp/urls.py
+++ SweCourseApp/urls.py
@@ -17,6 +17,7 @@ urlpatterns = [
     path('learningspace/<int:learning_space_id>/edittopic/<int:topic_id>',views.create_topic, name='edittopic' ),
     path('learningspace/<int:learning_space_id>/roadmap',views.road_map, name='roadmap'),
     path('learningspace/<int:learning_space_id>/topics',views.topics, name='topics'),
+    path('learningspace/<int:learning_space_id>/participants',views.participants, name='participants'),
     path('learningspace/<int:learning_space_id>/topic/<int:topic_id>',views.topic, name='topic'),
     path('ajax/postResource', views.postResource, name = 'postresource'),
     path('ajax/likeResource', views.likeResource, name = 'likeResource'),
